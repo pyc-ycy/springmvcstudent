df07b30c1e6a86c708a251e0e8be4428995edb24
7wc98#14 <2923616405@qq.com>
2021-01-07 11:19:37 +0800

    Initial commit

---
+++ src/test/java/com/pyc/springmvcstudent/SpringMvcStudentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pyc.springmvcstudent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcStudentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
